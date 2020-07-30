2428c35ef7f5bdd097a14a41d709c25dd7e5ed93
hhm19990929 <65931506+hhm19990929@users.noreply.github.com>
2020-07-30 14:31:23 +0800

    CanvasContext上下文

--- weixin_baidu/miniprogram/onekit/ui/canvas/canvas.js
+++ weixin_baidu/miniprogram/onekit/ui/canvas/canvas.js
@@ -1,48 +1,49 @@
 // onekit/ui/canvas/canvas.js
 Component({
-options: {
+    options: {
         addGlobalClass: true,
     },
-  data: {
+    data: {
 
-  },
-  properties: {     Id:{type:String,value:""},
-  Class:{type:String,value:""},
-  Style:{type:String,value:""},
-   
-    canvasId: {
-      type:String,
-      value:"",
     },
-    disableScroll: {
-      type:Boolean,
-      value:false,
+    properties: {
+        Id: { type: String, value: "" },
+        Class: { type: String, value: "" },
+        Style: { type: String, value: "" },
+
+        canvasId: {
+            type: String,
+            value: "",
+        },
+        disableScroll: {
+            type: Boolean,
+            value: false,
+        },
     },
-  },
-  methods: {
-    canvas_start(e){
-        console.log("canvas_start", e);
-        this.triggerEvent('touchstart',e.details)
-       },
-    canvas_move(e){
-        console.log("canvas_move", e);
-        this.triggerEvent('touchmove',e.details)
-       },
-    canvas_end(e){
-        console.log("canvas_end", e);
-        this.triggerEvent('touchend',e.details)
-       },
-    canvas_cancel(e){
-        console.log("canvas_cancel", e);
-        this.triggerEvent('touchcancel',e.details)
-       },
-    canvas_tap(e){
-        console.log("canvas_tap", e);
-        this.triggerEvent('longtap',e.details)
-       },
-    canvas_error(e){
-        console.log("canvas_error", e);
-        this.triggerEvent('error',e.details)
-       },
-  }
+    methods: {
+        canvas_start(e) {
+            console.log("canvas_start", e);
+            this.triggerEvent('touchstart', e.details)
+        },
+        canvas_move(e) {
+            console.log("canvas_move", e);
+            this.triggerEvent('touchmove', e.details)
+        },
+        canvas_end(e) {
+            console.log("canvas_end", e);
+            this.triggerEvent('touchend', e.details)
+        },
+        canvas_cancel(e) {
+            console.log("canvas_cancel", e);
+            this.triggerEvent('touchcancel', e.details)
+        },
+        canvas_tap(e) {
+            console.log("canvas_tap", e);
+            this.triggerEvent('longtap', e.details)
+        },
+        canvas_error(e) {
+            console.log("canvas_error", e);
+            this.triggerEvent('error', e.details)
+        },
+    }
 })
