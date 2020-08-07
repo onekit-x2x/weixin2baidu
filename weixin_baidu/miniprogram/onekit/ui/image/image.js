958d9e4611f807ad6fc1e63e49dc6d414dce6f31
gongyuan2 <939014677@qq.com>
2020-08-07 12:04:44 +0800

    imageHeightfix自定义

--- weixin_baidu/miniprogram/onekit/ui/image/image.js
+++ weixin_baidu/miniprogram/onekit/ui/image/image.js
@@ -1,34 +1,61 @@
+
+
 Component({
- options: {
+    options: {
         addGlobalClass: true,
+        virtualHost: true
     },
-    properties: {    
-     Id:{type:String,value:""},
-    Class:{type:String,value:""},
-    Style:{type:String,value:""},
-        src: { 
-            type: String, 
-            value: "", 
+
+    properties: {
+        Id: { type: String, value: "" },
+        Class: { type: String, value: "" },
+        Style: { type: String, value: "" },
+        src: {
+            type: String,
+            value: "",
         },
-        mode: { 
-            type: String, 
-            value: "scaleToFill", 
+        mode: {
+            type: String,
+            value: "scaleToFill",
         },
-        webp: { 
-            type: Boolean, 
-            value: false, 
+        webp: {
+            type: Boolean,
+            value: false,
         },
-        lazyLoad: { 
-            type: Boolean, 
-            value: false, 
+        lazyLoad: {
+            type: Boolean,
+            value: false,
         },
     },
+    data: { new_size: "" },
     methods: {
-       image_error(e){
-        this.triggerEvent('error',e.details)
-       },
-       image_load(e){
-        this.triggerEvent('load',e.details)
-       },
-    }
+        image_error(e) {
+            this.triggerEvent('error', e.details)
+        },
+        image_load(e) {
+            var that = this;
+            const SIZE = e.detail;
+            switch (this.properties.mode) {
+                case "heightFix":
+                    swan.createSelectorQuery()
+                        .in(this)
+                        .select('.onekit-image')
+                        .boundingClientRect(res => {
+                            const mode = "scaleToFill";
+                            const baidu_height = res.height;
+                            const baidu_width = SIZE.width * baidu_height / SIZE.height;
+                            const new_size = `width:${baidu_width}px;height:${baidu_height}px;`;
+                            that.setData({ mode, new_size });
+                        }).exec();
+                    break;
+                default:
+                    break;
+            }
+        },
+    },
+    lifetimes: {
+    },
 });
+
+
+
