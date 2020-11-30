b4edde1e0838ac46396963493191657081ce2e56
WangYeWei <826036140@qq.com>
2020-11-30 18:04:01 +0800

    .

--- smartprogram_dev/wang/wang.js
+++ smartprogram_dev/wang/wang.js
@@ -4,9 +4,10 @@ global = {};
 OnekitPage({
   data: {},
   onLoad: () => {
-    const arrayBuffer = new Uint8Array([11, 22, 33])
-    const base64 = wx.arrayBufferToBase64(arrayBuffer)
-    console.log(arrayBuffer)
-    console.log(base64)
+    wx.updateWeChatApp({
+      success: res => {
+        console.log(res)
+      }
+    })
   }
 });
