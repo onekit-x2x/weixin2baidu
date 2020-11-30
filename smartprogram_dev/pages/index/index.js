cdb2fef85435d358c6e780e5928287f1e3332988
lyx0lwz <1850115720@qq.com>
2020-11-30 11:49:36 +0800

    .

--- smartprogram_dev/pages/index/index.js
+++ smartprogram_dev/pages/index/index.js
@@ -1,31 +1,22 @@
-import {wx,OnekitPage} from "../../weixin2baidu"
-OnekitPage({
-    data: {
+/**
+ * @file index.js
+ * @author swan
+ */
+const app = getApp()
 
+Page({
+    data: {
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: swan.canIUse('button.open-type.getUserInfo')
     },
-    onLoad: function () {
+    onLoad() {
         // 监听页面加载的生命周期函数
-        wx.showLoading({title:"xx"})
     },
-    onReady: function() {
-        // 监听页面初次渲染完成的生命周期函数
-    },
-    onShow: function() {
-        // 监听页面显示的生命周期函数
-    },
-    onHide: function() {
-        // 监听页面隐藏的生命周期函数
-    },
-    onUnload: function() {
-        // 监听页面卸载的生命周期函数
-    },
-    onPullDownRefresh: function() {
-        // 监听用户下拉动作
-    },
-    onReachBottom: function() {
-        // 页面上拉触底事件的处理函数
-    },
-    onShareAppMessage: function () {
-        // 用户点击右上角转发
+    getUserInfo(e) {
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
+        });
     }
-});
+})
